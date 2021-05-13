9d558eef4dd39b7a767c92415afacdb476626301
Marc2816-esp <marc.28.dani@gmail.com>
2021-05-13 17:36:35 -0600

    Uso de props react

---
+++ src/components/Table.js
@@ -0,0 +1,17 @@
+import TableHeader from './TableHeader';
+import TableBody from './TableBody';
+
+const Table = (props) => {
+    const { charactersData, removeCharacter } =
+        props;
+    return (
+        <table>
+            <TableHeader />
+            <TableBody
+                charactersData={charactersData}
+                removeCharacter={removeCharacter}
+            />
+        </table>
+    );
+}
+export default Table;
